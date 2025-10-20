9eea32ddc5fda1aa661cde3394586c368f4b908b
Syed_Muhammad_Farzan_Obaid <k253042@nu.edu.pk>
2025-10-20 15:22:43 +0500

    Add files via upload

---
+++ Task#2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int num, mul, i;
+	printf("Enter a number: ");
+	scanf("%d", &num);
+	for(i=0;i<=10;i++){
+		mul=num*i;
+		printf("\n%d X %d = %d", num, i, mul);
+	}
+}
